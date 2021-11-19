0ff9e638fa2d132850032bc54cea2b9fa66e1642
icecreamIfrah <86468400+icecreamIfrah@users.noreply.github.com>
2021-11-19 10:58:54 +0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -70,9 +70,9 @@ function draw() {
       handleGameover(redBubbleGroup);
     }
     
-    /*if(blueBubbleGroup.collide(bulletGroup)){
+    if(blueBubbleGroup.collide(bulletGroup)){
       handleBubbleCollision(blueBubbleGroup);
-    }*/
+    }
 
     if(redBubbleGroup.collide(bulletGroup)){
       handleBubbleCollision(redBubbleGroup);
@@ -115,8 +115,8 @@ function handleBubbleCollision(bubbleGroup){
        score=score+1;
     }
 
-    /* blast= createSprite(bullet.x+60, bullet.y, 50,50);
-    blast.addImage(blastImg) */
+     blast= createSprite(bullet.x+60, bullet.y, 50,50);
+    blast.addImage(blastImg) 
 
     blast.scale=0.3
     blast.life=20
